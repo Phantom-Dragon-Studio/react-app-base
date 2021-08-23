47f718fbd826d2afc1931f58c735ac69e0d03a73
FullMe7alJacke7 <Thafireman@hotmail.com>
2021-08-23 16:36:01 -0400

    converted eslint to javascript to allow for conditional options and resolve a windows Vs Linux problem with line endings.

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+  },
+  extends: 'airbnb',
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: { jsx: true },
+    ecmaVersion: 2018,
+    allowImportExportEverywhere: true,
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'arrow-parens': 1,
+    'object-curly-newline': ['error', { multiline: true }],
+    'linebreak-style': ['error', (process.platform === 'win32' ? 'windows' : 'unix')],
+  },
+};
